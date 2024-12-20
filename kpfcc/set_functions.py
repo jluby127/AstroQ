faabc7bfc9a4d834d690cf2876d3f608dd5ac5f7
Jack Lubin <jlubin@uci.edu>
2024-12-20 12:30:59 -0800

    implementing new pandas querying scheme

--- kpfcc/set_functions.py
+++ kpfcc/set_functions.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
+# new set_functions.py with pandas commands
 
-def get_Dr(group_frame, r):
+def get_Dr(df, r):
     """
     Quick return the set of days d where request r has at least one available slot
 
@@ -8,9 +10,9 @@ def get_Dr(group_frame, r):
         - group_frame (obj) = a dataframe groupped by 'rr','dd','ss'
         - r (str) = the request ID
     """
-    return list(group_frame[group_frame.r==r].groupby('d').first().index)
+    return df['d'][df.r==r].unique()
 
-def get_Rds(group_frame, d, s):
+def get_Rds(df, d, s):
     """
     Quick return the requests r that are observable in slot s of day d
 
@@ -19,9 +21,9 @@ def get_Rds(group_frame, d, s):
         - d (int) = the day of the semester
         - s (int) = the slot in the night
     """
-    return list(group_frame[(group_frame.d==d) & (group_frame.s==s)].r)
+    return list(df['r'][(df.d==d) & (df.s==s)])#.sort()
 
-def get_Sr(group_frame, r):
+def get_Sr(df, r):
     """
     Quick return the set of d,s tuples where requests r is observable
 
@@ -29,9 +31,9 @@ def get_Sr(group_frame, r):
         - group_frame (obj) = a dataframe groupped by 'rr','dd','ss'
         - r (str) = the request ID
     """
-    return list(zip(list(group_frame[(group_frame.r==r)].d), list(group_frame[(group_frame.r==r)].s)))
+    return df[['d', 's']][df['r'] == r].values
 
-def get_Srd(group_frame, r, d):
+def get_Srd(df, r, d):
     """
     Quick return the set of slots in day d where request r is observable
 
@@ -40,9 +42,9 @@ def get_Srd(group_frame, r, d):
         - r (str) = the request ID
         - d (int) = the day of the semester
     """
-    return list(group_frame[(group_frame.r==r) & (group_frame.d==d)].s)
+    return df['s'][(df.r==r) & (df.d==d)]
 
-def get_Tds(group_frame, n_nights_in_semester, n_slots_in_night):
+def get_Tds(df, n_nights_in_semester, n_slots_in_night):
     """
     Quick return the set of slots/day pairs where at least one request is observable
 
@@ -53,50 +55,50 @@ def get_Tds(group_frame, n_nights_in_semester, n_slots_in_night):
     no = []
     for d in range(n_nights_in_semester):
         for s in range(n_slots_in_night):
-            val = get_Rds(group_frame, d, s)
+            val = get_Rds(df, d, s)
             if len(val) > 0:
                 yes.append([d, s])
             else:
                 no.append([d, s])
     return yes, no
 
-def get_Ir(group_frame):
+def get_Ir(df):
     """
     Quick return the set of requests that want multiple visits per night
 
     Args:
         - group_frame (obj) = a dataframe groupped by 'rr','dd','ss'
     """
-    return list(group_frame[group_frame.i>1].groupby('r').first().index)
+    return df['r'][df.i>1].unique()
 
-def get_Jr(group_frame):
+def get_Jr(df):
     """
     Quick return the set of requests that want only one visit per night
 
     Args:
         - group_frame (obj) = a dataframe groupped by 'rr','dd','ss'
     """
-    return list(group_frame[group_frame.i==1].groupby('r').first().index)
+    return list(df['r'][df.i==1].unique())
 
-def get_multi_slot_requests(group_frame):
+def get_multi_slot_requests(df):
     """
     Quick return the set of requests that want only one visit per night
 
     Args:
         - group_frame (obj) = a dataframe groupped by 'rr','dd','ss'
     """
-    return list(group_frame[group_frame.e>1].groupby('r').first().index)
+    return df['r'][df.e>1].unique()#.sort()
 
-def get_single_slot_requests(group_frame):
+def get_single_slot_requests(df):
     """
     Quick return the set of requests that want only one visit per night
 
     Args:
         - group_frame (obj) = a dataframe groupped by 'rr','dd','ss'
     """
-    return list(group_frame[group_frame.e==1].groupby('r').first().index)
+    return df['r'][df.e==1].unique()#.sort()
 
-def get_Rds_for_multislot(group_frame, d, s, explen):
+def get_Rds_for_multislot(df, d, s, explen):
     """
     Quick return all the tuple indices for range (r, d, s) to (r, d, s + e) for all r.
     Used in Contraint 2: Reserve slots for multi-slot exposure
@@ -109,7 +111,8 @@ def get_Rds_for_multislot(group_frame, d, s, explen):
     """
     all_indices = []
     for e in range(1, explen):
-        one_slot = list(group_frame[(group_frame.d==d) & (group_frame.s==s + e)].index)
+        one_slot = df[['r', 'd', 's']][(df.d==d) & (df.s==s + e)].values#.index
         for o in one_slot:
             all_indices.append(o)
+    all_indices = sorted(all_indices, key=lambda x: x[0])
     return all_indices
