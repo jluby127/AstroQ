c87a83c79fa1c6b2b1560e0b927effc6d030d619
Judah Van Zandt <judahvz@astro.ucla.edu>
2025-05-09 16:50:46 -0700

    new test function 'test_request_vs_schedule()' to compare the scheduled observations to the request set. Each column in the request set corresponds to a test in this function.

--- test_sample.py
+++ test_sample.py
@@ -9,46 +9,46 @@ import kpfcc.scheduler as sch
 
 class TestClass:
 
-    # def test_round2_weather(self):
-    #     dr.kpfcc_prep(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
-    #     dr.kpfcc_build(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
-    #     dr.schedule(argparse.Namespace(request_file="examples/hello_world/outputs/2024-08-01/request_set.json", config_file='examples/hello_world/config_hello_world.ini'))
-    #
-    # def test_bench(self):
-    #     # override the tests of "do these files exist" and just run it all
-    #     # but run a shortened version, so it doesn't take long, see shortcut=5
-    #
-    #
-    #     print("Running benchmark test.")
-    #
-    #     nR = 5
-    #     nS = 5
-    #     cf = 'examples/bench/config_benchmark.ini'
-    #
-    #     # Initialize manager and compute request set on the fly
-    #     # This is a hacky workaround. run_admin needs this file to exist. This can
-    #     # lead to race conditions if benchmarking is run in parallel.
-    #     config = ConfigParser()
-    #     config.read(cf)
-    #     upstream_path = eval(config.get('required', 'folder'), {"os": os})
-    #     semester_directory = upstream_path
-    #     requests_frame = bn.build_toy_model_from_paper(hours_per_program = 100)
-    #     if nR is not None:
-    #         requests_frame = requests_frame.iloc[:nR][::10] # short benchmark
-    #     requests_frame.to_csv(os.path.join(semester_directory, "inputs/Requests.csv"))
-    #     manager = mn.data_admin(cf)
-    #     manager.run_admin()
-    #
-    #     # Build observability maps and request set
-    #     print("Building valid indices.")
-    #     strategy, observable = rq.define_indices_for_requests(manager)
-    #     meta = rq.build_meta(cf)
-    #     request_set = rq.RequestSet(meta, strategy, observable)
-    #     current_day = str(config.get('required', 'current_day'))
-    #
-    #     # Run the schedule
-    #     schedule = sch.Scheduler(request_set, cf)
-    #     schedule.run_model()
+    def test_round2_weather(self):
+        dr.kpfcc_prep(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
+        dr.kpfcc_build(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
+        dr.schedule(argparse.Namespace(request_file="examples/hello_world/outputs/2024-08-01/request_set.json", config_file='examples/hello_world/config_hello_world.ini'))
+
+    def test_bench(self):
+        # override the tests of "do these files exist" and just run it all
+        # but run a shortened version, so it doesn't take long, see shortcut=5
+
+
+        print("Running benchmark test.")
+
+        nR = 5
+        nS = 5
+        cf = 'examples/bench/config_benchmark.ini'
+
+        # Initialize manager and compute request set on the fly
+        # This is a hacky workaround. run_admin needs this file to exist. This can
+        # lead to race conditions if benchmarking is run in parallel.
+        config = ConfigParser()
+        config.read(cf)
+        upstream_path = eval(config.get('required', 'folder'), {"os": os})
+        semester_directory = upstream_path
+        requests_frame = bn.build_toy_model_from_paper(hours_per_program = 100)
+        if nR is not None:
+            requests_frame = requests_frame.iloc[:nR][::10] # short benchmark
+        requests_frame.to_csv(os.path.join(semester_directory, "inputs/Requests.csv"))
+        manager = mn.data_admin(cf)
+        manager.run_admin()
+
+        # Build observability maps and request set
+        print("Building valid indices.")
+        strategy, observable = rq.define_indices_for_requests(manager)
+        meta = rq.build_meta(cf)
+        request_set = rq.RequestSet(meta, strategy, observable)
+        current_day = str(config.get('required', 'current_day'))
+
+        # Run the schedule
+        schedule = sch.Scheduler(request_set, cf)
+        schedule.run_model()
 
     """
     def test_prep(self):
@@ -57,8 +57,8 @@ class TestClass:
     """
 
 
-    # def test_plot(self):
-    #     dr.plot(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
+    def test_plot(self):
+        dr.plot(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
 
     # need environment variable to run
     """
@@ -85,6 +85,7 @@ class TestClass:
     def test_requests_vs_schedule(self):
         import kpfcc.request as req
         import pandas as pd
+        import numpy as np
         req = req.read_json('examples/hello_world/outputs/2024-08-01/request_set.json').strategy
         sch = pd.read_csv('examples/hello_world/outputs/2024-08-01/serialized_outputs_dense.csv')
         
@@ -94,69 +95,77 @@ class TestClass:
         #import pdb; pdb.set_trace()
         
         for star in req.id:
-            star_schedule = req.query(f"id=='{star}'") # Only the slots with the star listed
+            star_request = req.query(f"id=='{star}'")
+            star_schedule = sch.query(f"r=='{star}'") # Only the slots with the star listed
             
-        # 1) No stars scheduled during another star's slot
+        # 1) t-visit: No stars scheduled during another star's slot
             ## - This is already covered by first check above. Overlap isn't possible if all day/slot pairs are unique
             
             
-        # 2) Total number of nights in the semester is less than n_inter_max
-            n_inter_max = star_schedule['n_inter_max']
+            # import pdb; pdb.set_trace()
+        # 2) n_inter_max: Total number of nights a target is scheduled in the semester is less than n_inter_max
+            n_inter_max = star_request['n_inter_max'].values[0]
+            n_inter_sch = len(set(star_schedule.d)) # All unique nights with scheduled obs
 
-            n_inter_sch = len(star_schedule)/t_visit # Should be evenly divisible
+            # Now make sure the number of visits is less than the limit
+            n_inter_max_err = ("n_inter_max violated: "
+                              f"{star} is scheduled too many times in the semester "
+                              f"({n_inter_sch} > {n_inter_max})")
+            assert n_inter_sch <= n_inter_max, nim_err
             
-            assert np.isclose(n_inter_sch, int(n_inter_sch)) # First, require that n_inter_sch is an integer. If not, then it is not the case that every visit takes up t_visit slots
-            assert n_inter_sch <= n_inter_max # Now make sure the number of visits is less than the limit
             
-            
-            
-        # 3) N obs per day is between n_intra_min and n_intra_max
+        # 3) n_intra_min, n_intra_max: N obs per day is between n_intra_min and n_intra_max
             
             # Upper/lower limits on N obs per day
-            n_intra_min, n_intra_max = star_schedule[['n_intra_min', 'n_intra_max']].values[0]
+            n_intra_min, n_intra_max = star_request[['n_intra_min', 'n_intra_max']].values[0]
             
             # Scheduled min/max number of obs per day
-            n_intra_groupby = sch.query(f"id=='{star}'").groupby(['d']).size()
+            n_intra_groupby = star_schedule.groupby(['d']).size()
             n_intra_min_sch, n_intra_max_sch = n_intra_groupby.min(), n_intra_groupby.max()
             
-            import pdb; pdb.set_trace()
-            assert (n_intra_min <= n_intra_min_sch) & (n_intra_max_sch <= n_intra_max)
             
-        
+            # Ensure the target is never scheduled too few/many times in one night
+            n_intra_min_err = ("n_intra_min violated: "
+                              f"{star} is scheduled too few times in one night "
+                              f"({n_intra_min_sch} obs vs {n_intra_min} obs)")
+            assert n_intra_min <= n_intra_min_sch, n_intra_min_err
+            
+            n_intra_max_err = ("n_intra_max violated: "
+                              f"{star} is scheduled too many times in one night "
+                              f"({n_intra_max_sch} obs vs {n_intra_max} obs)")
+            assert n_intra_max_sch <= n_intra_max, n_intra_max_err
             
             
+        # 4) tau_inter: There must be at least tau_inter nights between successive observations of a target over the semester
+            tau_inter = star_request[['tau_inter']].values[0] # min num of nights before another obs
+
+            unique_days = np.sort(np.array(list(set(star_schedule.d))))
+            max_day_gaps = np.max(unique_days[1:] - unique_days[:-1])
+            
+            
+            if n_inter_max <= 1: # If only 1 obs per semester, no risk of spacing obs too closely
+                pass
+            else:
+                # All gaps are greater than the min gap 
+                tau_inter_err = ("tau_inter violated: "
+                                f"two obs of {star} are not spaced by enough days "
+                                f"({max_day_gaps} days vs {tau_inter} days)")
+                assert max_day_gaps >= tau_inter, tau_inter_err
         
         
-        # def visit_identifier(starname, schedule):
-        #     """
-        #     Find and group the slot indices of every
-        #     visit to a star in a schedule csv
-        #     """
-        #
-        #     #import pdb; pdb.set_trace()
-        #     all_slots = schedule.query(f"r=='{starname}'") # All slots assigned to this star
-        #
-        #     all_visits = []
-        #     for day in all_slots.d.values: # Each separate day
-        #         same_day_slots = all_slots.query(f"d=={day}").index # Indices of all slots in that day
-        #
-        #         same_day_visits = [[same_day_slots[0]]] # The first visit is the first filled slot
-        #
-        #         for slot in same_day_slots[1:]:
-        #             if slot == visits[-1][-1] + 1:
-        #                 same_day_visits[-1].append(slot) # If it's consecutive, add to the latest visit
-        #             else:
-        #                 same_day_visits.append([slot]) # If not, create new visit
-        #
-        #         all_visits.append([day, same_day_visits])
-        #
-        #     return all_visits
-        #
-        # # This block assumes each star is requested with the same requirements (t_visit, etc.). This need not be true. To modify, include in schedule file an index to link each obs to a specific request
-        # for starname in req.id:
-        #
-        #     all_visits = visit_identifier(starname, sch)
-        #     import pdb; pdb.set_trace()
+        # 5) tau_intra: There must be at least tau_intra slots between successive observations of a target in a single night
+            tau_intra = star_request[['tau_intra']].values[0] # min num of slots before another obs
+            
+            max_slot_diffs = star_schedule.groupby('d').s.diff() # Group by day, then find successive differences between slot numbers in the same day. Differences are not computed between the last slot of one night and the first slot of the next night (those values are NaN)
+            
+            if n_intra_max <= 1: # If only 1 obs per night, no risk of spacing obs too closely
+                pass
+            else:
+                tau_intra_err = ("tau_intra_violated: "
+                                f"two obs of {star} are not spaced by enough slots "
+                                f"({max_slot_diffs} vs {tau_intra})")
+                assert max_slot_diffs >= tau_intra, tau_intra_err
+            
     
         
 
