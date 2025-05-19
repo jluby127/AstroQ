143143f2cb252a11af7d23b843574bc6a5986b36
Judah Van Zandt <judahvz@astro.ucla.edu>
2025-05-19 16:01:18 -0700

    Added 'ns' argument to test_sample call to build_toy_model_from_paper function

--- test_sample.py
+++ test_sample.py
@@ -9,46 +9,46 @@ import kpfcc.scheduler as sch
 
 class TestClass:
 
-    # def test_round2_weather(self):
-    #     dr.kpfcc_prep(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
-    #     dr.kpfcc_build(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
-    #     dr.schedule(argparse.Namespace(request_file="examples/hello_world/outputs/2024-08-01/request_set.json", config_file='examples/hello_world/config_hello_world.ini'))
-    #
-    # def test_bench(self):
-    #     # override the tests of "do these files exist" and just run it all
-    #     # but run a shortened version, so it doesn't take long, see shortcut=5
-    #
-    #
-    #     print("Running benchmark test.")
-    #
-    #     nR = 5
-    #     nS = 5
-    #     cf = 'examples/bench/config_benchmark.ini'
-    #
-    #     # Initialize manager and compute request set on the fly
-    #     # This is a hacky workaround. run_admin needs this file to exist. This can
-    #     # lead to race conditions if benchmarking is run in parallel.
-    #     config = ConfigParser()
-    #     config.read(cf)
-    #     upstream_path = eval(config.get('required', 'folder'), {"os": os})
-    #     semester_directory = upstream_path
-    #     requests_frame = bn.build_toy_model_from_paper(hours_per_program = 100)
-    #     if nR is not None:
-    #         requests_frame = requests_frame.iloc[:nR][::10] # short benchmark
-    #     requests_frame.to_csv(os.path.join(semester_directory, "inputs/Requests.csv"))
-    #     manager = mn.data_admin(cf)
-    #     manager.run_admin()
-    #
-    #     # Build observability maps and request set
-    #     print("Building valid indices.")
-    #     strategy, observable = rq.define_indices_for_requests(manager)
-    #     meta = rq.build_meta(cf)
-    #     request_set = rq.RequestSet(meta, strategy, observable)
-    #     current_day = str(config.get('required', 'current_day'))
-    #
-    #     # Run the schedule
-    #     schedule = sch.Scheduler(request_set, cf)
-    #     schedule.run_model()
+    def test_round2_weather(self):
+        dr.kpfcc_prep(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
+        dr.kpfcc_build(argparse.Namespace(config_file='examples/hello_world/config_hello_world.ini'))
+        dr.schedule(argparse.Namespace(request_file="examples/hello_world/outputs/2024-08-01/request_set.json", config_file='examples/hello_world/config_hello_world.ini'))
+
+    def test_bench(self):
+        # override the tests of "do these files exist" and just run it all
+        # but run a shortened version, so it doesn't take long, see shortcut=5
+
+
+        print("Running benchmark test.")
+
+        nR = 5
+        nS = 5
+        cf = 'examples/bench/config_benchmark.ini'
+
+        # Initialize manager and compute request set on the fly
+        # This is a hacky workaround. run_admin needs this file to exist. This can
+        # lead to race conditions if benchmarking is run in parallel.
+        config = ConfigParser()
+        config.read(cf)
+        upstream_path = eval(config.get('required', 'folder'), {"os": os})
+        semester_directory = upstream_path
+        requests_frame = bn.build_toy_model_from_paper(nS, hours_per_program = 100)
+        if nR is not None:
+            requests_frame = requests_frame.iloc[:nR][::10] # short benchmark
+        requests_frame.to_csv(os.path.join(semester_directory, "inputs/Requests.csv"))
+        manager = mn.data_admin(cf)
+        manager.run_admin()
+
+        # Build observability maps and request set
+        print("Building valid indices.")
+        strategy, observable = rq.define_indices_for_requests(manager)
+        meta = rq.build_meta(cf)
+        request_set = rq.RequestSet(meta, strategy, observable)
+        current_day = str(config.get('required', 'current_day'))
+
+        # Run the schedule
+        schedule = sch.Scheduler(request_set, cf)
+        schedule.run_model()
 
     """
     def test_prep(self):
