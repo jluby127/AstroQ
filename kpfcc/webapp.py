3127f96906d53241b1765c89695effd1dfee48b7
Judah Van Zandt <judahvz@astro.ucla.edu>
2025-06-05 14:59:43 -0700

    New format for front page of kpfcc page

--- kpfcc/webapp.py
+++ kpfcc/webapp.py
@@ -1,3 +1,5 @@
+# webapp.py
+
 from flask import Flask, render_template, request
 import plotly.io as pio
 import plotly.graph_objs as go
@@ -8,59 +10,35 @@ import os
 app = Flask(__name__, template_folder="../templates")
 
 def cof_birdseye(program):
-    # file_root = "/Users/judahvz/research/code/GitHub/AstroQ/examples/hello_world/"
-    # config_file = os.path.join(file_root, "config_hello_world.ini")
-    # pkl_path = os.path.join(file_root, "reports/admin/2024-08-01/")
-    # data = pl.read_star_objects(pkl_path)
-    #
-    # manager = mn.data_admin(config_file)
-    # manager.run_admin()
-    #
-    # all_stars_list = [obj for obj_list in data[0].values() for obj in obj_list]
-
-    if program == 'All Stars COF':
-        return pl.cof_builder(all_stars_list, manager, flag=True)
-    elif program == 'All Programs COF':
+    # if program == 'All Stars COF':
+    #     return pl.cof_builder(all_stars_list, manager, flag=True)
+    if program == 'All Programs COF':
         return pl.cof_builder(list(data[1].values()), manager, flag=True)
     elif program == 'Birds Eye':
         return pl.generate_birds_eye(manager, data[2], all_stars_list)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    labels = ["All Stars COF", "All Programs COF", "Birds Eye"]
-    selected_label = request.form.get("plot_type", labels[0])  # Default to first
-    fig = cof_birdseye(selected_label)
-    fig_html = pio.to_html(fig, include_plotlyjs=False, full_html=False, div_id="main_plot")
-    
-    return render_template("index2.html", figure_html=fig_html, labels=labels, selected_label=selected_label)
-    
-    
+    pages = ["Overview", "Single Star"]
+    selected_page = request.form.get("page", pages[0])  # Default to "Overview"
+
+    figures_html = []
+
+    if selected_page == "Overview":
+        labels = ["All Programs COF", "Birds Eye"]
+        for label in labels:
+            fig = cof_birdseye(label)
+            fig_html = pio.to_html(fig, include_plotlyjs=False, full_html=False)
+            figures_html.append(fig_html)
+
+    return render_template("index1.html", figures_html=figures_html, pages=pages, selected_page=selected_page)
+
 def launch_app(pkl_path, config_file):
-    """
-    Wrapper for index() functions that
-    accepts arguments and defines global
-    variables for use in index()
-    """
-    
     global data, manager, all_stars_list
-    
+
     data = pl.read_star_objects(pkl_path)
-    
     manager = mn.data_admin(config_file)
     manager.run_admin()
-    
     all_stars_list = [obj for obj_list in data[0].values() for obj in obj_list]
-    
+
     app.run(debug=True)
-    
-    return
-
-if __name__ == "__main__":
-    
-    from kpfcc import _ROOT
-
-    pkl_path = os.path.join(_ROOT, "../examples/hello_world/reports/admin/2024-08-01/")
-    config_file = os.path.join(_ROOT, "../examples/hello_world/config_hello_world.ini")
-    
-    launch_app(pkl_path, config_file)
-
