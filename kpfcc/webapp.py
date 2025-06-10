360014b59733ea672c0e482153c21178f785bd8e
Judah Van Zandt <judahvz@astro.ucla.edu>
2025-06-10 14:17:35 -0700

    Adding multiple routes to web app for admin view, program view, etc.

--- kpfcc/webapp.py
+++ kpfcc/webapp.py
@@ -2,22 +2,20 @@
 
 from flask import Flask, render_template, request
 import plotly.io as pio
-import plotly.graph_objs as go
 import kpfcc.plot as pl
 import kpfcc.management as mn
-import os
 
 app = Flask(__name__, template_folder="../templates")
 
-def cof_birdseye(program):
-    # if program == 'All Stars COF':
-    #     return pl.cof_builder(all_stars_list, manager, flag=True)
-    if program == 'All Programs COF':
-        return pl.cof_builder(list(data[1].values()), manager, flag=True)
-    elif program == 'Birds Eye':
-        return pl.generate_birds_eye(manager, data[2], all_stars_list)
+# Plot-building helper functions
+def make_cof():
+    return pl.cof_builder(list(data[1].values()), manager, flag=True)
 
-@app.route("/", methods=['GET', 'POST'])
+def make_birdseye():
+    return pl.generate_birds_eye(manager, data[2], all_stars_list)
+
+# Shared homepage view
+@app.route("/", methods=["GET", "POST"])
 def index():
     pages = ["Overview", "Single Star"]
     selected_page = request.form.get("page", pages[0])  # Default to "Overview"
@@ -25,14 +23,40 @@ def index():
     figures_html = []
 
     if selected_page == "Overview":
-        labels = ["All Programs COF", "Birds Eye"]
+        labels = ["All Programs COF", "Bird's Eye"]
         for label in labels:
-            fig = cof_birdseye(label)
+            fig = make_cof() if label == "All Programs COF" else make_birdseye()
             fig_html = pio.to_html(fig, include_plotlyjs=False, full_html=False)
             figures_html.append(fig_html)
 
-    return render_template("index1.html", figures_html=figures_html, pages=pages, selected_page=selected_page)
+    return render_template("index.html", figures_html=figures_html, pages=pages, selected_page=selected_page)
 
+# Admin page route
+@app.route("/admin")
+def admin():
+    fig1 = make_cof()
+    fig2 = make_birdseye()
+    figures_html = [
+        pio.to_html(fig1, include_plotlyjs=False, full_html=False),
+        pio.to_html(fig2, include_plotlyjs=False, full_html=False)
+    ]
+    return render_template("admin.html", figures_html=figures_html)
+
+# Observer page route
+@app.route("/observer")
+def observer():
+    fig = make_birdseye()
+    figure_html = [pio.to_html(fig, include_plotlyjs=False, full_html=False)]
+    return render_template("observer.html", figure_html=figure_html)
+
+# Program page route
+@app.route("/program")
+def program():
+    fig = make_cof()
+    figure_html = [pio.to_html(fig, include_plotlyjs=False, full_html=False)]
+    return render_template("program.html", figure_html=figure_html)
+
+# Entry point for launching the app
 def launch_app(pkl_path, config_file):
     global data, manager, all_stars_list
 
