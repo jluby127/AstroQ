a3ba2220bfb3860a3c0f2dc813bc03b1949846d4
Erik Petigura <petigura@astro.ucla.edu>
2025-07-29 16:16:57 -0700

    cleaning up RequestSEt

--- astroq/request.py
+++ astroq/request.py
@@ -40,7 +40,7 @@ class RequestSet(object):
         self.meta = {"s1_time":daily_starting_time, "d1_date":current_day, "slot_duration":slot_size}
         
         # Build strategy and observable data
-        strategy, observable = define_indices_for_requests(manager)
+        strategy, observable = self._define_indices_for_requests(manager)
         
         # enforce order
         strategy_cols = 'id t_visit n_intra_min n_intra_max tau_intra n_inter_max tau_inter'.split()
@@ -48,89 +48,48 @@ class RequestSet(object):
         self.strategy = strategy
         self.observability = observable
 
-    def __str__(self, n=10):
-        s = "# Request Set # \n"
-        s += "## Meta Data ## \n"
-        s += '\n'*2
-        s += self.meta.to_string()
-        s += '\n'*2
-        s += "## Strategy ##\n"
-        s += self.strategy.head(n).to_string()
-        s += '\n'*2
-        s += "## Observable ##\n"
-        s += self.observability.head(n).to_string()
-        return s
-
-    def to_json(self, fn):
+    def _define_indices_for_requests(self, manager):
         """
-        Save request set to json
+        Using the dictionary of indices where each request is available, define a dataframe for which
+        we will use to cut/filter/merge r,d,s tuples
         """
-        data = {}
-        if isinstance(self.meta, dict):
-            data['meta'] = self.meta
-        else:
-            data['meta'] = self.meta.to_dict()
-        data['strategy'] = self.strategy.to_dict()
-        data['observable'] = self.observability.to_dict()
+        # Create Access object with required parameters
+        access_obj = ac.Access(
+            semester_start_date=manager.semester_start_date,
+            semester_length=manager.semester_length,
+            slot_size=manager.slot_size,
+            observatory=manager.observatory,
+            current_day=manager.current_day,
+            all_dates_dict=manager.all_dates_dict,
+            custom_file=manager.custom_file,
+            allocation_file=manager.allocation_file,
+            past_history=manager.past_history,
+            today_starting_night=manager.today_starting_night,
+            slots_needed_for_exposure_dict=manager.slots_needed_for_exposure_dict
+        )
+        access = access_obj.produce_ultimate_map(manager.requests_frame)
+        # Convert to list of available indices
+        available_indices_for_request = access_obj.extract_available_indices_from_record(access, manager.requests_frame)
 
-        with open(fn, "w") as f:
-            json.dump(data,f,indent=4) # Pretty-printed for readability
-
-def read_json(fn):
-    """
-    Read request set from json
-    """
-    with open(fn, "r") as f:
-        data = json.load(f)
-
-    meta = pd.Series(data['meta'])
-    strategy = pd.DataFrame(data['strategy'])
-    observable = pd.DataFrame(data['observable'])
-    rs = RequestSet(meta, strategy, observable)
-    return rs
-
-def define_indices_for_requests(manager):
-    """
-    Using the dictionary of indices where each request is available, define a dataframe for which
-    we will use to cut/filter/merge r,d,s tuples
-    """
-    # Create Access object with required parameters
-    access_obj = ac.Access(
-        semester_start_date=manager.semester_start_date,
-        semester_length=manager.semester_length,
-        slot_size=manager.slot_size,
-        observatory=manager.observatory,
-        current_day=manager.current_day,
-        all_dates_dict=manager.all_dates_dict,
-        custom_file=manager.custom_file,
-        allocation_file=manager.allocation_file,
-        past_history=manager.past_history,
-        today_starting_night=manager.today_starting_night,
-        slots_needed_for_exposure_dict=manager.slots_needed_for_exposure_dict
-    )
-    access = access_obj.produce_ultimate_map(manager.requests_frame)
-    # Convert to list of available indices
-    available_indices_for_request = access_obj.extract_available_indices_from_record(access, manager.requests_frame)
-
-    observability_keys = []
-    strategy_keys = []
-    for n,row in manager.requests_frame.iterrows():
-        name = row['starname']
-        if name in list(available_indices_for_request.keys()):
-            max_n_visits = int(row['n_intra_max'])
-            min_n_visits = int(row['n_intra_min'])
-            intra = int(row['tau_intra'])
-            nnights = int(row['n_inter_max'])
-            inter = int(row['tau_inter'])
-            slots_needed = manager.slots_needed_for_exposure_dict[name]
-            strategy_keys.append([name, slots_needed, min_n_visits, max_n_visits, intra, nnights, inter])
-            for d in range(len(available_indices_for_request[name])):
-                for s in available_indices_for_request[name][d]:
-                    observability_keys.append((name, d, s))
-    strategy = pd.DataFrame(strategy_keys, columns =['id', 't_visit', 'n_intra_min', 'n_intra_max',
-                                                     'tau_intra', 'n_inter_max', 'tau_inter'])
-    observability = pd.DataFrame(observability_keys, columns =['id', 'd', 's'])
-    return strategy, observability
+        observability_keys = []
+        strategy_keys = []
+        for n,row in manager.requests_frame.iterrows():
+            name = row['starname']
+            if name in list(available_indices_for_request.keys()):
+                max_n_visits = int(row['n_intra_max'])
+                min_n_visits = int(row['n_intra_min'])
+                intra = int(row['tau_intra'])
+                nnights = int(row['n_inter_max'])
+                inter = int(row['tau_inter'])
+                slots_needed = manager.slots_needed_for_exposure_dict[name]
+                strategy_keys.append([name, slots_needed, min_n_visits, max_n_visits, intra, nnights, inter])
+                for d in range(len(available_indices_for_request[name])):
+                    for s in available_indices_for_request[name][d]:
+                        observability_keys.append((name, d, s))
+        strategy = pd.DataFrame(strategy_keys, columns =['id', 't_visit', 'n_intra_min', 'n_intra_max',
+                                                         'tau_intra', 'n_inter_max', 'tau_inter'])
+        observability = pd.DataFrame(observability_keys, columns =['id', 'd', 's'])
+        return strategy, observability
 
 def cull_from_weather(request_set, weather_loss_map):
     request_set.original_observability = request_set.observability
