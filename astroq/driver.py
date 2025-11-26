437ce8c0505e72ee8ef4905d6b7c2f200c630d6f
Jack Lubin <jlubin@uci.edu>
2025-11-26 10:27:10 -0500

    deleting unnecessary print statements in driver for recompute exptimes

--- astroq/driver.py
+++ astroq/driver.py
@@ -158,13 +158,6 @@ def kpfcc_prep(args):
         slowdown_factors = {1: 1.0, 2: 2.0, 3: 4.0}
         slow = slowdown_factors[band_number]
         new_exptimes = ob.recompute_exposure_times(filtered_good_obs, slow)
-        for i in range(len(filtered_good_obs)):
-            if filtered_good_obs['program_code'][i] == "2025B_N427":
-                print("exp meter threshold: ", filtered_good_obs['exp_meter_threshold'][i])
-                print("old exptime: ", filtered_good_obs['exptime'][i])
-                print("new exptime: ", new_exptimes[i])
-                print("factor: ", np.round(filtered_good_obs['exptime'][i]/new_exptimes[i], 2))
-                print("--------------------------------")
         filtered_good_obs['exptime'] = new_exptimes
         filtered_good_obs.to_csv(savepath + request_file, index=False)
     
