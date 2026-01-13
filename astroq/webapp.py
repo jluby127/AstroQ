e0cc77230868b26e7650923c22cf8fe2579150ba
Jack Lubin <jlubin@uci.edu>
2026-01-13 10:54:26 -0800

    started, not finished, building the rawobs tracker

--- astroq/webapp.py
+++ astroq/webapp.py
@@ -189,6 +189,7 @@ def render_admin_page():
     fig_tau_inter_line = pl.get_tau_inter_line(semester_planner, all_stars_from_all_programs, use_program_colors=True)
     fig_timebar = pl.get_timebar(semester_planner, all_stars_from_all_programs, use_program_colors=True)
     fig_timebar_by_program = pl.get_timebar_by_program(semester_planner, data_astroq[0])
+    fig_rawobs = pl.get_rawobs(semester_planner, all_stars_from_all_programs)
 
     fig_cof_html = pio.to_html(fig_cof, full_html=True, include_plotlyjs='cdn')
     fig_birdseye_html = pio.to_html(fig_birdseye, full_html=True, include_plotlyjs='cdn')
@@ -196,8 +197,9 @@ def render_admin_page():
     fig_tau_inter_line_html = pio.to_html(fig_tau_inter_line, full_html=True, include_plotlyjs='cdn')
     fig_timebar_html = pio.to_html(fig_timebar, full_html=True, include_plotlyjs='cdn')
     fig_timebar_by_program_html = pio.to_html(fig_timebar_by_program, full_html=True, include_plotlyjs='cdn')
+    fig_rawobs_html = pio.to_html(fig_rawobs, full_html=True, include_plotlyjs='cdn')
 
-    figures_html = [fig_timebar_html, fig_timebar_by_program_html, fig_cof_html, fig_birdseye_html, fig_tau_inter_line_html, fig_football_html]
+    figures_html = [fig_timebar_html, fig_timebar_by_program_html, fig_rawobs_html, fig_cof_html, fig_birdseye_html, fig_tau_inter_line_html, fig_football_html]
 
     return render_template("admin.html", tables_html=[request_table_html], figures_html=figures_html, timestamp=semester_planner_timestamp)
 
@@ -223,14 +225,16 @@ def render_program_page(semester_code, date, band, program_code):
     fig_tau_inter_line = pl.get_tau_inter_line(semester_planner, program_stars)
     fig_football = pl.get_football(semester_planner, program_stars)
     fig_timebar = pl.get_timebar(semester_planner, program_stars, use_program_colors=True)
+    fig_rawobs = pl.get_rawobs(semester_planner, program_stars)
 
     fig_cof_html = pio.to_html(fig_cof, full_html=True, include_plotlyjs='cdn')
     fig_birdseye_html = pio.to_html(fig_birdseye, full_html=True, include_plotlyjs='cdn')
     fig_tau_inter_line_html = pio.to_html(fig_tau_inter_line, full_html=True, include_plotlyjs='cdn')
     fig_football_html = pio.to_html(fig_football, full_html=True, include_plotlyjs='cdn')
     fig_timebar_html = pio.to_html(fig_timebar, full_html=True, include_plotlyjs='cdn')
+    fig_rawobs_html = pio.to_html(fig_rawobs, full_html=True, include_plotlyjs='cdn')
 
-    figures_html = [fig_timebar_html, fig_cof_html, fig_birdseye_html, fig_tau_inter_line_html, fig_football_html]
+    figures_html = [fig_timebar_html, fig_rawobs_html, fig_cof_html, fig_birdseye_html, fig_tau_inter_line_html, fig_football_html]
     
     return render_template("semesterplan.html", 
                          programname=program_code, 
@@ -264,14 +268,16 @@ def render_star_page(starname):
                 fig_birdseye = pl.get_birdseye(semester_planner, data_astroq[2], [star_obj])
                 fig_tau_inter_line = pl.get_tau_inter_line(semester_planner, [star_obj])
                 fig_football = pl.get_football(semester_planner, [star_obj])
+                fig_rawobs = pl.get_rawobs(semester_planner, [star_obj])
 
                 fig_cof_html = pio.to_html(fig_cof, full_html=True, include_plotlyjs='cdn')
                 fig_birdseye_html = pio.to_html(fig_birdseye, full_html=True, include_plotlyjs='cdn')
                 fig_tau_inter_line_html = pio.to_html(fig_tau_inter_line, full_html=True, include_plotlyjs='cdn')
                 fig_football_html = pio.to_html(fig_football, full_html=True, include_plotlyjs='cdn')
+                fig_rawobs_html = pio.to_html(fig_rawobs, full_html=True, include_plotlyjs='cdn')
 
                 tables_html = [request_table_html]
-                figures_html = [fig_cof_html, fig_birdseye_html, fig_tau_inter_line_html, fig_football_html]
+                figures_html = [fig_rawobs_html, fig_cof_html, fig_birdseye_html, fig_tau_inter_line_html, fig_football_html]
 
                 return render_template("star.html", starname=true_starname, tables_html=tables_html, figures_html=figures_html, timestamp=semester_planner_timestamp)
     
