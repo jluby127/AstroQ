updating with respect to fixing throttle. now hours per night is standardized in creating the programs.csv file. now slew overhead is included in time to slots calculation. now plot.py time bar includes all overheads

astroq/splan.py
@@ -327,8 +327,8 @@ class SemesterPlanner(object):
         slots_needed_for_exposure_dict = {}
         for n, row in self.requests_frame.iterrows():
             starid = row['unique_id']
-            exposure_time = float(row['exptime'])
-            overhead = 45*float(row['n_exp'] - 1) #+ 180*float(row['n_intra_max'])
+            exposure_time = float(row['exptime']*row['n_exp'])
+            overhead = 45*float(row['n_exp'] - 1) + 180*float(row['n_intra_max'])
             
             if always_round_up_flag:
                 slots_needed = int(np.ceil((exposure_time + overhead) / (self.slot_size * 60.0)))